2e8781efebf00643b4e5ec7bd1e8ee3f17ee7055
jahprakash <jahnaviprakash9@gmail.com>
2025-11-12 09:51:47 +0530

    Create sum of natural numbers.py

---
+++ sum of natural numbers.py
@@ -0,0 +1,11 @@
+# sum of first natural numbers
+num = int(input("enter a number"))
+if num < 0:
+  print("enter a number")
+else:
+  sum_numbers = 0
+  counter = 1
+  while counter <= num:
+    sum_numbers += counter
+    counter += 1
+  print("the sum of natural number is ",sum_numbers)
