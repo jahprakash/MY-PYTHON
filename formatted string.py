af546254831272c2703a0678ba1e93419c5d667f
jahprakash <jahnaviprakash9@gmail.com>
2025-11-12 10:17:08 +0530

    Create formatted string.py

---
+++ formatted string.py
@@ -0,0 +1,6 @@
+# formatted string
+name="jahnavi"
+age="18"
+college="shridevi"
+department="aiml"
+print(f"my name is: {name} and my age is: {age} i am studying in: {college} and my department is: {department}")
