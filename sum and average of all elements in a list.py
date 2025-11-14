3ea0c01a7024b4bc2efc20ea9de8577ba67c35b3
jahprakash <jahnaviprakash9@gmail.com>
2025-11-14 15:12:18 +0530

    Create sum and average of all elements in a list.py

---
+++ sum and average of all elements in a list.py
@@ -0,0 +1,8 @@
+#sum and average of all elements in a list
+my_list = [10, 20, 30, 40, 50]
+
+list_sum = sum(my_list)
+list_average = list_sum 
+
+print("The sum of the elements in the list is:", list_sum)
+print("The average of the elements in the list is:", list_average)
