64e5f8fa310816daa460dbbf456fd10c1166c3ac
jahprakash <jahnaviprakash9@gmail.com>
2025-11-14 15:22:24 +0530

    Create list in ascending and descending order.py

---
+++ list in ascending and descending order.py
@@ -0,0 +1,11 @@
+#list in ascending and descending order
+my_list = [64,25,12,22,11]
+my_list.sort()
+print(f"list in ascending order: {my_list}")
+my_list.sort(reverse=True)
+print(f"list in descending order: {my_list}"
+my_list = [64,25,12,22,11]
+my_list.sort()
+print(f"list in ascending order: {my_list}")
+my_list.sort(reverse=True)
+print(f"list in descending order: {my_list}")
