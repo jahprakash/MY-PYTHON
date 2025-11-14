4c3a4687289e42237c87a9626462af61a39cbebf
jahprakash <jahnaviprakash9@gmail.com>
2025-11-14 15:13:58 +0530

    Create count elements occurs in a list.py

---
+++ count elements occurs in a list.py
@@ -0,0 +1,5 @@
+# count elements occurences in a list
+my_list = [1,2,3,4,1,4,4,5,6,1]
+element_to_count = 1
+count = my_list.count(element_to_count)
+print(f"The element {element_to_count} appears {count} times in the list.")
